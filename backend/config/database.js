67d3f83fd2a5feb5c8e76592bd3dc4ba0ec14fab
abdo20010dx <abdo20010dx@gmail.com>
2021-08-08 23:08:01 +0200

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,13 +2,16 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        uri: 'mongodb+srv://tester:test@cluster0.cmpu2.mongodb.net/strapi?retryWrites=true&w=majority',
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'mysql-42579-0.cloudclusters.net'),
+        port: env.int('DATABASE_PORT', 17401),
+        database: env('DATABASE_NAME', 'abdo'),
+        username: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', '80ww4PNS'),
       },
-      options: {
-        ssl: true,
-      },
+      options: {},
     },
   },
 });
