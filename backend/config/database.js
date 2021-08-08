cf5ac47a2b8026750f7091297ff23d25767fb625
abdo20010dx <abdo20010dx@gmail.com>
2021-08-08 20:57:01 +0200

    database integration publish

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,12 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'mysql-42579-0.cloudclusters.net'),
+        port: env.int('DATABASE_PORT', 17401),
+        database: env('DATABASE_NAME', 'abdo'),
+        username: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', '80ww4PNS'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
+
