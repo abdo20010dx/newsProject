37142e28283e358e18e5f7d6a172421c10048f26
abdo20010dx <abdo20010dx@gmail.com>
2021-08-09 03:26:11 +0200

    cloudinary package and plugin

---
+++ backend/config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+    
+    upload: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name:'abdo20010dx',
+          api_key: '444143915891998',
+          api_secret: 'XNF_KNthsDEYFG7RvM724wpDb5w'
+        }
+      },
+  });
